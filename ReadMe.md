7b01a10662be4677d4b4a9f0d1dcf3def412dac0
MisinformedOwl <jaydean1311@gmail.com>
2026-05-18 17:12:37 +0100

    First stage done
    + Collecting candidate names and images and storing correctly.

---
+++ ReadMe.md
@@ -0,0 +1,9 @@
+# The Candidate Detector
+
+### Why did i make this?
+Me and some friends were on a website where we had to guess based off a picture, which political party a candidate belonged to. And throughout this i could not stop thinking about if i could make convolutional neural network detect this, and then further more. Find out what party it thinks me and my friends are part off.
+
+### Where did i get the data?
+I am using a csv file acquired from https://candidates.democracyclub.org.uk/data/?election_date=2026-05-07&election_id=%5Elocal.*&format=csv&field_group=results. This csv file provides more than enough information for me. I have the candidate party membership, their id for marking images with, and a link to their image.
+
+## This is very much still in development. Will add more as it goes.
--- getCandidateImages.py
+++ getCandidateImages.py
@@ -1,7 +1,47 @@
 import urllib.request
+import os
+import pandas as pd
+from time import sleep
+import random
 
 def getImage(link: str, id: str):
     urllib.request.urlretrieve(link, f"Candidates/{id}.jpg")
 
+
+def checkFolderExists():
+    path = os.getcwd()+"\\Candidates"
+    print(f"Checking for: {path}")
+    if not os.path.isdir(path):
+        os.mkdir(path)
+
+
+def loadData() -> pd.DataFrame:
+    return pd.read_csv("candidatesData.csv")
+
+
+def collectImages(data: pd.DataFrame):
+    wantedParties = set({"Conservative and Unionist Party", "Reform UK", "Green Party", "Labour Party", "Liberal Democrats"})
+    newData = pd.DataFrame(columns=["id", "name", "party"])
+    try:
+        for _, candidate in data.iterrows():
+            if pd.isna(candidate["image"]):
+                print(f"No image for candidate {candidate["person_id"]}")
+                continue
+            if not candidate["party_name"] in wantedParties:
+                print(f"Unwanted party for candidate {candidate["person_id"]}")
+                continue
+            newData.loc[len(newData)] = [candidate["person_id"], candidate["person_name"], candidate["party_name"]]
+            print(candidate["image"])
+            getImage(candidate["image"], candidate["person_id"])
+            sleep(1+random.random()/2)
+    except KeyboardInterrupt:
+        print("Ending search.")
+    except Exception as e:
+        print(f"Unknown exception {e.__class__.__name__}")
+    newData.to_csv("candidates.csv")
+
+
 if __name__ == "__main__":
-    getImage("https://candidates.democracyclub.org.uk/media/images/people/82796/b91d8409-bc0a-4520-93ba-813f34ff1118.png", 0)
+    checkFolderExists()
+    data = loadData()
+    collectImages(data)
