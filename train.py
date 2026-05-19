dc700548d3bf2ade9b203f25953a176ef3cd93d6
MisinformedOwl <jaydean1311@gmail.com>
2026-05-19 18:06:32 +0100

    + Fixed issues regarding the dataset not being standardised. It now correctly loads into the dataloader.

--- train.py
+++ train.py
@@ -1,8 +1,14 @@
 import torch
+from torch import Generator
+from torch.optim import SGD
+import torch.nn as nn
+import torch.nn.functional as F
 import os
-from torch.utils.data import Dataset, DataLoader
+from torch.utils.data import Dataset, DataLoader, random_split
 from PIL import Image
 import pandas as pd
+import torchvision
+from torchvision.transforms import v2
 
 #------------------------------------Globals----------------------------------------------
 
@@ -11,6 +17,9 @@ partyDict = {"Conservative and Unionist Party" : 1, "Reform UK" : 2, "Green Part
 #-----------------------------------Classes-----------------------------------------------
 
 class candidateDataset(Dataset):
+    """
+    Creates a custom dataset compatible with torch's DataLoader to avoid putting too much pressure on my ram when training potentially thousands of images.
+    """
     def __init__(self, paths: list[str], labels, transform=None):
         self.imagePaths = paths
         self.labels = labels
@@ -20,29 +29,78 @@ class candidateDataset(Dataset):
         return len(self.imagePaths)
 
     def __getitem__(self, index):
+        """
+        Grabs the image path and opens the image to convert it to a image and then a tensor.
+        """
         path = self.imagePaths[index]
-        image = Image.open(path).convert("RGB")
-        label = partyDict[self.labels[index]]
-
+        #Read image, set to RGB to avoid alpha channel. Then convert to a float so it can be normalised for use in a tensor.
+        image = torchvision.io.read_image(path, mode=torchvision.io.ImageReadMode.RGB).float() / 255.0
+        resize = v2.Resize((224,224), antialias=True)
+        image = resize(image)
         if self.transform:
             image = self.transform(image)
+
+        label = partyDict[self.labels[index]]
+
         
         return image, torch.tensor(label)
 
+
+class ModelBuild(nn.Module):
+    """
+    Responcible for building the neural network.
+    """
+    def __init__(self):
+        super().__init__()
+
+        self.conv1 = nn.Conv2d(2,3,5)
+        self.pool = nn.MaxPool2d(2,2)
+    
+    def forward(self, x):
+        """
+        This is where the data is put forward.
+        """
+        x = self.pool(F.leaky_relu(self.conv1(x)))
+        x = self.pool(F.leaky_relu(self.conv2(x)))
+        return x
+
 #---------------------------------Functions--------------------------------------
 
-def testgen():
-    for index in range(len(dataset)):
-        print(f"Index now at {index}")
-        data = dataset.__getitem__(index)
-        print(data)
-        yield data[0], data[1]
+def loadDataset() -> candidateDataset:
+    print("Loading csv.")
+    data = pd.read_csv("candidates.csv", index_col=0)
+    return candidateDataset(data["imageLoc"],data["party"])
 
 #----------------------------Main------------------------------------------
 
-dir = "./Candidates"
+if __name__ == "__main__":
+    dataset = loadDataset()
+    trainData, testData = random_split(dataset, [0.8,0.2], generator=Generator().manual_seed(42))
 
-print("Loading csv.")
-data = pd.read_csv("candidates.csv", index_col=0)
+    trainLoader = DataLoader(
+        dataset=trainData,
+        batch_size=32,
+        num_workers=2,
+        shuffle = True
+    )
 
-dataset = candidateDataset(data["imageLoc"],data["party"])
+    model = ModelBuild()
+
+    lossfn = nn.CrossEntropyLoss()
+    epochs = 10
+    lr= 0.001
+    optim = SGD(model.parameters(),lr=lr, momentum=0.9)
+
+    model.train()
+    for e in range(epochs):
+        for batch in trainLoader:
+            print(type(batch), batch)
+            os.exit(0)
+            images, labels = batch
+            optim.zero_grad()
+            # get results
+            outputs = model(batch)
+            loss = lossfn(outputs, labels)
+            loss.backward()
+            optim.step()
+            #loss data analysis.
